307da4e471963f6f8196b34652fad1ebb55cc3af
Fansink <flansink@kza.nl>
2016-10-25 19:02:02 +0200

    Eerste Commit van de code

---
+++ Nummer Tovenaar/Assets/Scripts/NummerTovenaar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NummerTovenaar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Welkom bij de Nummer Tovenaar!");
+		print("Neem een nummer in je gedachten, maar vertel hem niet!!");
+
+		int max = 1000;
+		int min = 1;
+
+		print ("Het hoogste getal dat je mag kiezen is " + max);
+		print ("Het laagste getal dat je mag kiezen is " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
