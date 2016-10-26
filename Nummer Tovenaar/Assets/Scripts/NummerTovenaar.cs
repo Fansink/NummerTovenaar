a46fc5050c7d9a1296e69c1ec3b2d19a2263b9be
Fansink <flansink@kza.nl>
2016-10-26 18:59:23 +0200

    Laatste commit NummerTovenaar
    Cursus afgemaakt en Random nummers kiezen toegevoegd

--- Nummer Tovenaar/Assets/Scripts/NummerTovenaar.cs
+++ Nummer Tovenaar/Assets/Scripts/NummerTovenaar.cs
@@ -3,20 +3,62 @@ using System.Collections;
 
 public class NummerTovenaar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		print("Welkom bij de Nummer Tovenaar!");
-		print("Neem een nummer in je gedachten, maar vertel hem niet!!");
+    // Use this for initialization
+    int max;
+    int min;
+    int guess;
+    int nummer;
 
-		int max = 1000;
-		int min = 1;
+    void Start ()
+    {
+        StartGame();
+    }
 
-		print ("Het hoogste getal dat je mag kiezen is " + max);
-		print ("Het laagste getal dat je mag kiezen is " + min);
-	}
-	
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = Random.Range(min, max);
+
+        print("==============================");
+        print("Welkom bij de Nummer Tovenaar!");
+        print("Neem een nummer in je gedachten, maar vertel hem niet!!");
+
+        print("Het hoogste getal dat je mag kiezen is " + max);
+        print("Het laagste getal dat je mag kiezen is " + min);
+
+        print("Is het getal hoger of lager dan " + guess + "?");
+        print("Pijltje omhoog voor hoger, Pijltje omlaag voor lager en enter als het getal gelijk is.");
+
+        max = max + 1;
+    }
+
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            WillekeurigNummer();
+            min = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            WillekeurigNummer();
+            max = guess;
+            NextGuess();
+        } else if (Input.GetKeyDown(KeyCode.Return)) {
+            print("Jeeej ik heb gewonnen!!");
+            StartGame();
+        }
+    }
+
+    void WillekeurigNummer()
+    {
+        guess = Random.Range(min, max);
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        print("Is het getal hoger of lager dan " + guess + "?");
+        print("Pijltje omhoog voor hoger, Pijltje omlaag voor lager en enter als het getal gelijk is.");
+    }
 }
