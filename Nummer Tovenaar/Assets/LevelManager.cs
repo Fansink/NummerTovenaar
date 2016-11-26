a1cc8591eb4b563e9a6b33a9073e43ad56b27968
Fansink <flansink@kza.nl>
2016-11-26 18:51:20 +0100

    Nieuwe scenes en UI toegevoegd

---
+++ Nummer Tovenaar/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void StartGame(string name)
+    {
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame(string name)
+    {
+        Application.Quit();
+        //Debug.Log("I want to quit");
+    }
+
+}
